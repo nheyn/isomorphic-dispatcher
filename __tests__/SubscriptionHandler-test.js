10851a1a42135ae35938344ac1e4f44436758575
nheyn <nick@heyn.io>
2015-09-25 15:31:56 -0600

    Come Up with Tests for Subscription Handler
    Create list of test for the SubscriptionHandler class.

--- __tests__/SubscriptionHandler-test.js
+++ __tests__/SubscriptionHandler-test.js
@@ -1,4 +1,18 @@
 var SubscriptionHandler = require.requireActual('../src/SubscriptionHandler');
 
 describe('SubscriptionHandler', () => {
+	it('can add subscribers', () => {
+		//TODO, nyi: add mulitple subscribers
+		//TODO, nyi: add non-function to handler
+	});
+
+	it('can remove subscribers', () => {
+		//TODO, nyi: remove subscribers
+		//TODO, nyi: remove from empty handler
+	});
+
+	it('can publish a value', () => {
+		//TODO, nyi: subscribers get published value
+		//TODO, nyi: error thrown in subscription
+	});
 });
