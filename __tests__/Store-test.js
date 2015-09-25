f8cbc34f3ad71c7b1cce3d17a1c781af5b8221b6
nheyn <nick@heyn.io>
2015-09-25 15:49:50 -0600

    Come Up with Tests for Store
    Create list of test for the Store class.

--- __tests__/Store-test.js
+++ __tests__/Store-test.js
@@ -1,4 +1,27 @@
 var Store = require.requireActual('../src/Store');
 
 describe('Store', () => {
+	it('has the given initial state', () => {
+		//TODO, nyi: single updater
+		//TODO, nyi: mulitple updaters
+		//TODO, nyi: error from updater
+	});
+
+	it('has the returned state after dispatch is called' () => {
+		//TODO, nyi: single updater
+		//TODO, nyi: mulitple updaters
+		//TODO, nyi: error from updater
+	});
+
+	it('can start the dispatch call in middle', () => {
+		//TODO, nyi: has correct arg
+		//TODO, nyi: start in middle
+	});
+
+	it('validates method arguments', () => {
+		//TODO, nyi: checks action is a plain object
+		//TODO, nyi: start after last dispacther
+		//TODO, nyi: has invalid start point when starting in middle
+		//TODO, nyi: updater returns no state during dispatch
+	});
 });
