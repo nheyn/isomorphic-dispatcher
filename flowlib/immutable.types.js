eaf30e539e5787b520f95fb96800bf8d8684bbb0
nheyn <nick@heyn.io>
2015-10-10 14:38:05 -0600

    Update Dispatcher to Immutable JS.
    Update Dispatcher to use Immutable JS.

--- flowlib/immutable.types.js
+++ flowlib/immutable.types.js
@@ -9,5 +9,19 @@ declare module "immutable" {
 		indexOf(searchValue: T): number;
 		reduce<R>(reducer: (reduction: R, value: T, index: number) => R, initialReduction?: R): R;
 		forEach(sideEffect: (value: T, index: number) => void): void;
+		toJS(): Array<T>;
+	}
+
+	declare class Map<K, V> {
+		static <K, V>(obj?: {[key: K]: V}): Map<K, V>;
+		set(key: K, value: V): Map<K, V>;
+		get(key: K): V;
+		keys(): Iterable<number, K>;
+		values(): Iterable<number, V>;
+		has(key: K): boolean;
+		includes(value: V): boolean;
+		map<M>(mapper: (value: V, key: K) => M): Map<K, M>;
+		forEach(sideEffect: (value: V, key: K) => void): void;
+		toJS(): {[key: K]: V};
 	}
 }
