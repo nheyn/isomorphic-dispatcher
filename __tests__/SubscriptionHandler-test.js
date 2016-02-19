1ebd666e5400bb90ba08af77ea8ec334be09243f
nheyn <nick@heyn.io>
2016-02-18 21:36:47 -0700

    Fill in Subscription Handler Tests
    Implement the tests for the SubscriptionHandler class.

--- __tests__/SubscriptionHandler-test.js
+++ __tests__/SubscriptionHandler-test.js
@@ -1,17 +1,72 @@
+jest.dontMock('immutable');
 jest.dontMock('../src/SubscriptionHandler');
 
 var SubscriptionHandler = require('../src/SubscriptionHandler');
 
 describe('SubscriptionHandler', () => {
 	it('can add subscribers', () => {
-		//TODO, rewrite test
+		const subscribers = [
+			jest.genMockFunction(),
+			jest.genMockFunction(),
+			jest.genMockFunction()
+		];
+
+		SubscriptionHandler.createSubscriptionHandler()
+			.subscribe(subscribers[0])
+			.subscribe(subscribers[1])
+			.subscribe(subscribers[2])
+			.publish('some value');
+
+		subscribers.forEach((subscriberFunc) => {
+			const { mock } = subscriberFunc;
+
+			expect(mock.calls.length).toBe(1);
+		});
 	});
 
-	it('can remove subscribers', () => {
-		//TODO, rewrite test
+	it('can remove a subscriber', () => {
+		const subscribers = [
+			jest.genMockFunction(),
+			jest.genMockFunction(),
+			jest.genMockFunction()
+		];
+
+		const indexToRemove = 1;
+		let subsciptionHandler = SubscriptionHandler.createSubscriptionHandler()
+									.subscribe(subscribers[0])
+									.subscribe(subscribers[1])
+									.subscribe(subscribers[2])
+		subsciptionHandler.publish('some value');
+
+		subsciptionHandler = subsciptionHandler.unsubscribe(subscribers[indexToRemove])
+		subsciptionHandler.publish('some value');
+
+
+		subscribers.forEach((subscriberFunc, index) => {
+			const { mock } = subscriberFunc;
+
+			expect(mock.calls.length).toBe(index === indexToRemove? 1: 2);
+		});
 	});
 
 	it('can publish a value', () => {
-		//TODO, rewrite test
+		const subscribers = [
+			jest.genMockFunction(),
+			jest.genMockFunction(),
+			jest.genMockFunction()
+		];
+		const publishedValue = 'published value';
+
+		SubscriptionHandler.createSubscriptionHandler()
+			.subscribe(subscribers[0])
+			.subscribe(subscribers[1])
+			.subscribe(subscribers[2])
+			.publish(publishedValue);
+
+		subscribers.forEach((subscriberFunc) => {
+			const { mock } = subscriberFunc;
+
+			expect(mock.calls[0][0]).toEqual(publishedValue);
+		});
 	});
 });
