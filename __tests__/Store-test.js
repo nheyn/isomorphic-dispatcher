06e6935706675c8ac906ecffe0c666b5395dbe34
nheyn <nick@heyn.io>
2016-02-18 21:16:40 -0700

    Fill in Store Tests
    Implement the tests for the Store class.

--- __tests__/Store-test.js
+++ __tests__/Store-test.js
@@ -1,53 +1,329 @@
 jest.dontMock('../src/Store');
+jest.dontMock('immutable');
 
-var Store = require('../src/Store');
+const Store = require('../src/Store');
 
 describe('Store', () => {
-	it('will call updaters passed to register method with the action passed to dispatch', () => {
-		//TODO, rewrite test
+	pit('will call updaters passed to register method with the action passed to dispatch', () => {
+		const initialState = { the: 'state of the store' };
+		const dispatchedAction = { the: 'dispatched action' };
+		const store = Store.createStore(initialState).register((state, action) => {
+			expect(action).toEqual(dispatchedAction);
+
+			return state;
+		}).register((state, action) => {
+			expect(action).toEqual(dispatchedAction);
+
+			return state;
+		}).register((state, action) => {
+			expect(action).toEqual(dispatchedAction);
+
+			return state;
+		});
+
+		return store.dispatch(dispatchedAction);
 	});
 
-	it('can replace the state before starting to dispatch', () => {
-		//TODO, rewrite test
+	pit('can replace the state before starting to dispatch', () => {
+		const initialState = { the: 'state of the store' };
+		const replacedState = { the: 'replaced state' };
+		const store = Store.createStore(initialState).register((state, action) => {
+			expect(state).toEqual(replacedState);
+
+			return state;
+		});
+
+		return store.dispatch({ type: 'testAction' }, {
+			replaceState: replacedState
+		});
 	});
 
-	it('can skip updaters at the beginning of the dispatch', () => {
-		//TODO, rewrite test
+	pit('can skip updaters at the beginning of the dispatch', () => {
+		const initialState = { the: 'state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const wrongUpdatedState = { the: 'wrong updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action) => {
+			expect('not').toBe('called');
+
+			return wrongUpdatedState;
+		}).register((state, action) => {
+			expect('not').toBe('called');
+
+			return wrongUpdatedState;
+		}).register((state, action) => {
+			return updatedState;
+		});
+
+		const updatedStorePromise = store.dispatch({ type: 'testAction' }, {
+			skip: 2
+		});
+
+		return updatedStorePromise.then((newStore) => {
+			const newState = newStore.getState();
+
+			expect(newState).toEqual(updatedState);
+			expect(newState).not.toEqual(wrongUpdatedState);
+		});
 	});
 
-	it('will call the onSever arg of the updaters if no client side settings are given', () => {
-		//TODO, rewrite test
+	pit('will call the onServer arg of the updaters if no client side settings are given', () => {
+		const initialState = { the: 'initial state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action, onServer) => {
+			return onServer(() => {
+				return updatedState;
+			});
+		});
+
+		return store.dispatch({ type: 'testAction' }).then((updatedStore) => {
+			expect(updatedStore.getState()).toEqual(updatedState);
+		});
 	});
 
-	it('will call the onSever arg of the updaters if the arg is given', () => {
-		//TODO, rewrite test
+	pit('will call the onServer arg of the updaters if the arg is given', () => {
+		const initialState = { the: 'initial state of the store' };
+		const passedArg = { the: 'passed arg' };
+		const store = Store.createStore(initialState).register((state, action, onServer) => {
+			return onServer((arg) => {
+				expect(arg).toBe(passedArg);
+
+				return state;
+			});
+		});
+
+		return store.dispatch({ type: 'testAction' }, {
+			arg: passedArg
+		});
 	});
 
-	it('will call the finishOnServer function if given and onServer is called', () => {
-		//TODO, rewrite test
+	pit('will get the state from finishOnServer function if given and onServer is called', () => {
+		const initialState = { the: 'state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const wrongUpdatedState = { the: 'wrong updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action, onServer) => {
+			return onServer(() => {
+				return wrongUpdatedState;
+			});
+		});
+
+		const updatedStorePromise = store.dispatch({ type: 'testAction' }, {
+			finishOnServer() {
+				return updatedState;
+			}
+		});
+
+		return updatedStorePromise.then((newStore) => {
+			const newState = newStore.getState();
+
+			expect(newState).toEqual(updatedState);
+			expect(newState).not.toEqual(wrongUpdatedState);
+		});
 	});
 
-	it('will not call the finishOnServer function if given and onServer is not called', () => {
-		//TODO, rewrite test
+	pit('will call the finishOnServer function with the correct stopping point', () => {
+		const initialState = { the: 'state of the store' };
+		const store = Store.createStore(initialState).register((state, action) => {
+			return state;
+		}).register((state, action, onServer) => {
+			return onServer(() => state);
+		}).register((state, action) => {
+			expect('not').toBe('called');
+
+			return state;
+		});
+
+		const finishOnServerFunc = jest.genMockFunction().mockReturnValue(initialState);
+		const updatedStorePromise = store.dispatch({ type: 'testAction' }, {
+			finishOnServer: finishOnServerFunc
+		});
+
+		return updatedStorePromise.then((newStore) => {
+			const { mock } = finishOnServerFunc;
+
+			expect(mock.calls.length).toBe(1);
+			expect(mock.calls[0]).toEqual([initialState, 1]);
+		});
 	});
 
-	it('will call finishedUpdaters after all the updater have been called in the store', () => {
-		//TODO, rewrite test
+	pit('will not call the finishOnServer function if given and onServer is not called', () => {
+		const initialState = { the: 'state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const wrongUpdatedState = { the: 'wrong updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action, onServer) => {
+			return updatedState;
+		});
+
+		const updatedStorePromise = store.dispatch({ type: 'testAction' }, {
+			finishOnServer() {
+				expect('not').toBe('called');
+
+				return wrongUpdatedState;
+			}
+		});
+
+		return updatedStorePromise.then((newStore) => {
+			const newState = newStore.getState();
+
+			expect(newState).toEqual(updatedState);
+			expect(newState).not.toEqual(wrongUpdatedState);
+		});
+	});
+
+	pit('will call finishedUpdaters after all the updater have been called in the store', () => {
+		const initialState = { the: 'state of the store' };
+		const store = Store.createStore(initialState).register((state, action, onServer) => state);
+
+		const finishedUpdatersFunc = jest.genMockFunction();
+		const updatedStorePromise = store.dispatch({ type: 'testAction' }, {
+			finishedUpdaters: finishedUpdatersFunc
+		});
+
+		return updatedStorePromise.then(() => {
+			const { mock } = finishedUpdatersFunc;
+
+			expect(mock.calls.length).toBe(1);
+		});
 	});
 
 	it('can replace the state of the store, keeping the same updaters', () => {
-		//TODO, rewrite test
+		const initialState = { the: 'state of the store' };
+		const updaters = [
+			jest.genMockFunction().mockReturnValue(initialState),
+			jest.genMockFunction().mockReturnValue(initialState),
+			jest.genMockFunction().mockReturnValue(initialState)
+		];
+		const store = Store.createStore(initialState).register(updaters[0]).register(updaters[1]).register(updaters[2]);
+
+		const replacedState = { the: 'replaced state of the store' };
+		const storeWithReplacedState = store.replaceState(replacedState);
+		expect(storeWithReplacedState.getState()).toEqual(replacedState);
+
+		return storeWithReplacedState.dispatch({ type: 'testAction' }).then((updatedStore) => {
+			expect(updatedStore.getState()).toEqual(initialState);
+
+			updaters.forEach((updaterFunc) => {
+				const { mock } = updaterFunc;
+
+				expect(mock.calls.length).toBe(1);
+			})
+		});
 	});
 
-	it('will get the initial state before dispatch is called', () => {
-		//TODO, rewrite test
+	it('will have the initial state before dispatch is called', () => {
+		const state = { the: 'state of the store' };
+		const store = Store.createStore(state);
+
+		expect(store.getState()).toEqual(state);
 	});
 
-	it('will get the initial state during a dispatch', () => {
-		//TODO, rewrite test
+	pit('will not change its state, because Store is immutable', () => {
+		const initialState = { the: 'initial state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action) => updatedState);
+
+		expect(store.getState()).toEqual(initialState);
+		return store.dispatch({ type: 'testAction' }).then(() => {
+			expect(store.getState()).toEqual(initialState);
+		});
 	});
 
-	it('will get the new state after the dispatch has finished', () => {
-		//TODO, rewrite test
+	pit('will return a Store with the new state from dispatch', () => {
+		const initialState = { the: 'initial state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action) => updatedState);
+
+		return store.dispatch({ type: 'testAction' }).then((newStore) => {
+			expect(newStore.getState()).toEqual(updatedState);
+		});
+	});
+
+	pit('can accumulate state from multiple updaters', () => {
+		const initialState = { initial: 'initial' };
+		const stateUpdates = [
+			{ update: 0, val0: 0 },
+			{ update: 1, val1: 1 },
+			{ update: 2, val2: 2 }
+		];
+		const store = Store.createStore(initialState).register((state, action) => {
+			return Object.assign({}, state, stateUpdates[0]);
+		}).register((state, action) => {
+			return Object.assign({}, state, stateUpdates[1]);
+		}).register((state, action) => {
+			return Object.assign({}, state, stateUpdates[2]);
+		});
+
+		return store.dispatch({ type: 'testAction' }).then((newStore) => {
+			const updatedState = newStore.getState();
+
+			// Check initial state is still their
+			expect(updatedState.initial).toBe(initialState.initial);
+
+			// Check add state is their
+			expect(updatedState.val0).toBe(0);
+			expect(updatedState.val1).toBe(1);
+			expect(updatedState.val2).toBe(2);
+
+			// Check overwritten state is their and correct
+			expect(updatedState.update).toBe(2);
+		});
+	});
+
+	pit('can accumulate state from multiple calls to dispatch', () => {
+		const initialState = { initial: 'initial' };
+		const store = Store.createStore(initialState).register((state, action) => {
+			const count = state.count? state.count: 0;
+
+			return Object.assign({}, state, { count: count+1 });
+		});
+
+		let updatedStorePromise = Promise.resolve(store);
+
+		const repeatCount = 5;
+		for(let i = 0; i<repeatCount; i++) {
+			updatedStorePromise = updatedStorePromise.then((updatedStore) => {
+				return updatedStore.dispatch({ type: 'testAction' });
+			});
+		}
+
+		return updatedStorePromise.then((updatedStore) => {
+			const updatedState = updatedStore.getState();
+
+			// Check initial state is still their
+			expect(updatedState.initial).toBe(initialState.initial);
+
+			// Check count is correct
+			expect(updatedState.count).toBe(repeatCount);
+		});
+	});
+
+	pit('can wait for asynchronous updaters', () => {
+		const initialState = { the: 'initial state of the store' };
+		const updatedState = { the: 'updated state of the store' };
+		const store = Store.createStore(initialState).register((state, action) => {
+			return new Promise((resolve) => {
+				resolve(updatedState);
+
+				//TODO, timeout function is not ever called (over 5000 msec)
+				/*setTimeout(() => {
+					resolve(updatedState)
+				}, 50);*/
+			});
+		});
+
+		/*let didFinish = false;
+		let didCheck = false;
+		setTimeout(() => {
+			expect(didFinish).toBe(false);
+
+			didCheck = true;
+		}, 25);*/
+
+
+		return store.dispatch({ type: 'testAction' }).then((newStore) => {
+			expect(newStore.getState()).toEqual(updatedState);
+
+			/*expect(didCheck).toBe(true);
+			didFinish = true;*/
+		});
 	});
 });
